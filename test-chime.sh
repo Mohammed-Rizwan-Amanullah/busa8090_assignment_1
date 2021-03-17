6a52cebc589f0d764167972bd0d5c88a8c1c6dc5
mohrizwan1993 <mohrizwan1993@email.com>
2021-03-17 20:17:37 +0000

    finished question 3

--- test-chime.sh
+++ test-chime.sh
@@ -3,11 +3,11 @@
 #testing time and using chime sound
 hour=$(date +%I)
 minutes=$(date +%M)
-if [ "$minutes" -le 20 ]; then
+if [ "$minutes" -le 19 ]; then
   echo "no chime"
-elif [ "$minutes" -le 40 ]; then
+elif [ "$minutes" -le 39 ]; then
   echo -e "\a"
-elif [ "$minutes" -le 60 ]; then
+elif [ "$minutes" -le 59 ]; then
 	echo -e "\a"
 	sleep 1 # sleep for one second
 	echo -e "\a"
